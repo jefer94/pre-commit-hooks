7c3914c2228720a179dc0a30bbe10288925faca4
jefer94 <jdefreitaspinto@gmail.com>
2022-08-03 14:30:45 -0500

    creating a custom hook about pipenv

---
+++ pre_commit_hooks/pipenv_requirements_fixer.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import argparse
+import os
+import subprocess
+from typing import IO
+from typing import Sequence
+
+
+def fix_file() -> int:
+    try:
+        with open('requirements.txt', 'r') as f:
+            content = f.read()
+    except FileNotFoundError:
+        content = ''
+
+    subprocess.run(['pipenv', 'install'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = subprocess.run(['pipenv', 'run', 'pip', 'freeze', '>', 'requirements.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if content != result.stdout:
+        with open('requirements.txt', 'wb') as f:
+            f.write(result.stdout)
+
+        return 1
+
+    return 0
+
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to fix')
+    args = parser.parse_args(argv)
+
+    retv = 0
+
+    if args.filenames:
+        ret_for_file = fix_file()
+        if ret_for_file:
+            print('Fixing requirements.txt')
+
+        retv |= ret_for_file
+
+    return retv
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
